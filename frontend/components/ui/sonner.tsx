6d554ca6f174c980d0e0ffb9c5c05caa6c9a77e4
Pedro Pablo Osorio San Martin <pedro.osorio04@inacapmail.com>
2026-01-31 20:54:00 -0300

    Se crea la primera pagina del dahsboard y se hicieron las pruebas de funcionamiento de el login, el register no se hizo dado a que se piensa orientar el registro de usuarios mas a admin desde el lado del backend

--- frontend/components/ui/sonner.tsx
+++ frontend/components/ui/sonner.tsx
@@ -16,7 +16,6 @@ const Toaster = ({ ...props }: ToasterProps) => {
   return (
     <Sonner
       theme={theme as ToasterProps["theme"]}
-      className="toaster group"
       icons={{
         success: <CircleCheckIcon className="size-4" />,
         info: <InfoIcon className="size-4" />,
@@ -24,14 +23,11 @@ const Toaster = ({ ...props }: ToasterProps) => {
         error: <OctagonXIcon className="size-4" />,
         loading: <Loader2Icon className="size-4 animate-spin" />,
       }}
-      style={
-        {
-          "--normal-bg": "var(--popover)",
-          "--normal-text": "var(--popover-foreground)",
-          "--normal-border": "var(--border)",
-          "--border-radius": "var(--radius)",
-        } as React.CSSProperties
-      }
+      toastOptions={{
+        classNames: {
+          description: "text-gray-700 opacity-100",
+        },
+      }}
       {...props}
     />
   )
