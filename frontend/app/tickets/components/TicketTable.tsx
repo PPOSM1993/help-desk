86a74b137bfd77dd51adc47e9fa696bf1d02bf74
Pedro Pablo Osorio San Martin <pedro.osorio04@inacapmail.com>
2026-02-02 15:24:46 -0300

    Se trabaja en el frontend de tickets

---
+++ frontend/app/tickets/components/TicketTable.tsx
@@ -0,0 +1,9 @@
+export default function TicketTable() {
+    return (
+        <>
+            <div>
+                Ticket Table
+            </div>
+        </>
+    )
+}
--- frontend/app/tickets/page.tsx
+++ frontend/app/tickets/page.tsx
@@ -1,9 +1,12 @@
-export default function TicketsPage() {
-    return(
-        <>
-        <div className="">
-            <h1>Tickets</h1>
-        </div>
-        </>
-    )
+import TicketTable from "./components/TicketTable"
+
+export default async function TicketsPage() {
+
+
+  return (
+    <div className="space-y-4">
+      <h1 className="text-2xl font-bold">Tickets</h1>
+      <TicketTable  />
+    </div>
+  )
 }
