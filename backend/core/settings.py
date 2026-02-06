9b5cd785414ba5c6cb477dd8f5c25c205076f88b
Pedro Pablo Osorio San Martin <pedro.osorio04@inacapmail.com>
2026-02-06 16:57:58 -0300

    se cambia algo en el settings

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -115,7 +115,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = "es-CL"
+LANGUAGE_CODE = "en-us"
 
 TIME_ZONE = "America/Santiago"
 
