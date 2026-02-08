c2292f6ef9ec08d336c13eb5413ed8444419dbea
Pedro Pablo Osorio San Martin <pedro.osorio04@inacapmail.com>
2026-02-08 13:59:36 -0300

    Se Guardan cambios pendientes

--- frontend/app/tickets/components/TicketActions.tsx
+++ frontend/app/tickets/components/TicketActions.tsx
@@ -2,30 +2,40 @@
 
 import { Ticket } from "../types/ticket"
 import { Button } from "@/components/ui/button"
-import { MoreHorizontal, Pencil, Trash2Icon } from "lucide-react"
-
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
+import { Pencil, Trash2Icon } from "lucide-react"
+import { useRouter } from "next/navigation"
+import { deleteTicket } from "../services/tickets.server"
 
 type Props = {
   ticket: Ticket
 }
 
 export function TicketActions({ ticket }: Props) {
+  const router = useRouter()
+
+
+  async function handleTicket() {
+    if (!confirm("¿Estás seguro que quieres eliminar este ticket?")) return
+
+    await deleteTicket(ticket.id)
+    router.refresh()
+  }
+
+
   return (
-    <>
-      <div className="flex gap-2">
-        <Button variant="ghost" size="icon" className="bg-yellow-500 hover:bg-yellow-500 rounded-none ">
-          <Pencil className="h-4 w-4 text-white" />
-        </Button>
-        <Button variant="ghost" size="icon" className="bg-red-500 hover:bg-red-500 rounded-none">
-          <Trash2Icon className="h-4 w-4 text-white" />
-        </Button>
-      </div>
-    </>
+    <div className="flex gap-2">
+      <Button variant="ghost" size="icon" className="bg-yellow-500 hover:bg-yellow-600">
+        <Pencil className="h-4 w-4 text-white" />
+      </Button>
+
+      <Button
+        variant="ghost"
+        size="icon"
+        className="bg-red-500 hover:bg-red-600"
+        onClick={handleTicket}
+>
+        <Trash2Icon className="h-4 w-4 text-white" />
+      </Button>
+    </div>
   )
 }
