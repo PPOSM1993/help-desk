d1d1f88139e0a231d59969e3d946f8f946603590
Pedro Pablo Osorio San Martin <pedro.osorio04@inacapmail.com>
2026-02-11 18:29:47 -0300

    cambios en los permisos de usuarios

--- backend/apps/authentication/models.py
+++ backend/apps/authentication/models.py
@@ -52,7 +52,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     role = models.CharField(
         max_length=20,
         choices=ROLE_CHOICES,
-        default='client'
+        default='admin'
     )
     email = models.EmailField(unique=True)
     username = models.CharField(max_length=150, unique=True)
